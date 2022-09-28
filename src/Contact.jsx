b68a194e470cfa211fb06c7f2a0de28dc764ea57
Habibjatala <=>
2022-09-29 02:50:20 +0500

    h1

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact = (props)=>{
     return(
-        <h1> Heloow This is the {props.name} page </h1>
+        <h1> Heloow This is the kjahfdkjahf {props.name} page </h1>
     )
 }
 export default Contact;
